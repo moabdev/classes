09049dc80a8a2a7b05e32898ceea7f962425d41c
moabdev <macenamoab3@gmail.com>
2024-10-26 20:21:59 -0300

    aula-express

---
+++ aula-express/exercicio1.js
@@ -0,0 +1,16 @@
+//importando modulo http, como se fosse uma biblioteca
+const server = require("http");
+
+server.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/plain")
+
+    if (req.url === "/") {
+        res.writeHead(200)
+        res.end("Sucesso");
+
+    }
+})
+
+server.listen("3000", () => {
+    console.log("criei meu primeiro servidor!", "e vou trabalhar no itau!"); 
+})
